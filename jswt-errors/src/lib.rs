0c15a4bc6b32e2301de4c70053fbd61e596fe8ad
Nish Tahir <nishtahir@outlook.com>
2021-12-18 17:53:26 -0500

    Refactor error handling into error emitter

--- jswt-errors/src/lib.rs
+++ jswt-errors/src/lib.rs
@@ -1,83 +1,58 @@
 mod codeframe;
+mod emitter;
 mod highlighter;
 
 use colored::*;
+use emitter::ErrorEmitter;
+use std::{borrow::Cow, collections::HashMap};
+
+use jswt_common::Span;
 use jswt_parser::ParseError;
 use jswt_semantics::*;
 use jswt_tokenizer::TokenizerError;
-use std::collections::HashMap;
+
+pub struct DiagnosticMessage {
+    level: Level,
+    message: Cow<'static, str>,
+    span: Span,
+    hint: Option<Cow<'static, str>>,
+}
 
 pub enum Level {
     Error,
     Warning,
 }
 
-use crate::{
-    codeframe::{code_frame, location_from_offset, Location, NodeLocation},
-    highlighter::highlight,
-};
-
 pub fn print_semantic_error(error: &SemanticError, source_map: &HashMap<String, &'static str>) {
-    match error {
-        SemanticError::VariableNotDefined { name, span } => {
-            let file = &span.file.to_string();
-            let source = source_map[file];
-            let start = location_from_offset(source, span.start);
-            let end = location_from_offset(source, span.end);
-            let header = create_header(file, &start, Level::Error);
-
-            let error_span = NodeLocation { end, start };
-            let frame = create_codeframe(
-                source,
-                error_span,
-                &format!("Variable '{}' was not defined in this scope", name),
-            );
-            println!("{}\n{}\n", header, frame);
-        }
-        SemanticError::VariableAlreadyDefined { name, span } => {
-            let file = &span.file.to_string();
-            let source = source_map[file];
-            let start = location_from_offset(source, span.start);
-            let end = location_from_offset(source, span.end);
-            let header = create_header(file, &start, Level::Error);
-
-            let error_span = NodeLocation { end, start };
-            let frame = create_codeframe(
-                source,
-                error_span,
-                &format!("Variable '{}' was already defined in this scope", name),
-            );
-            println!("{}\n{}\n", header, frame);
-        }
-        SemanticError::FunctionAlreadyDefined { name, span } => {
-            let file = &span.file.to_string();
-            let source = source_map[file];
-            let start = location_from_offset(source, span.start);
-            let end = location_from_offset(source, span.end);
-            let header = create_header(file, &start, Level::Error);
-
-            let error_span = NodeLocation { end, start };
-            let frame = create_codeframe(
-                source,
-                error_span,
-                &format!("Function '{}' was already defined in this scope", name),
-            );
-            println!("{}\n{}\n", header, frame);
-        }
+    let diagnostic = match error {
+        SemanticError::VariableNotDefined { name, span } => DiagnosticMessage {
+            level: Level::Error,
+            span: span.clone(),
+            message: format!("Variable '{}' was not defined in this scope", name).into(),
+            hint: None,
+        },
+        SemanticError::VariableAlreadyDefined { name, span } => DiagnosticMessage {
+            level: Level::Error,
+            span: span.clone(),
+            message: format!("Variable '{}' was already defined in this scope", name).into(),
+            hint: None,
+        },
+        SemanticError::FunctionAlreadyDefined { name, span } => DiagnosticMessage {
+            level: Level::Error,
+            span: span.clone(),
+            message: format!("Function '{}' was already defined in this scope", name).into(),
+            hint: None,
+        },
         SemanticError::NotAFunctionError { span, name_span } => {
             let file = &span.file.to_string();
             let source = source_map[file];
-            let start = location_from_offset(source, span.start);
-            let end = location_from_offset(source, span.end);
-            let header = create_header(file, &start, Level::Error);
-            let error_span = NodeLocation { end, start };
             let offending_token = &source[name_span.start..name_span.end];
-            let frame = create_codeframe(
-                source,
-                error_span,
-                &format!("'{}' is not a function.", offending_token),
-            );
-            println!("{}\n{}\n", header, frame);
+            DiagnosticMessage {
+                level: Level::Error,
+                span: span.clone(),
+                message: format!("'{}' is not a function.", offending_token).into(),
+                hint: None,
+            }
         }
         SemanticError::TypeError {
             span,
@@ -86,130 +61,85 @@ pub fn print_semantic_error(error: &SemanticError, source_map: &HashMap<String, 
         } => {
             let file = &span.file.to_string();
             let source = source_map[file];
-            let start = location_from_offset(source, span.start);
-            let end = location_from_offset(source, span.end);
-
-            let header = create_header(file, &start, Level::Error);
-            let error_span = NodeLocation { end, start };
             let offending_token = &source[offending_token.start..offending_token.end];
-            let frame = create_codeframe(
-                source,
-                error_span,
-                &format!(
-                    "Type Error: '{}' is not a {} expression.",
+            DiagnosticMessage {
+                level: Level::Error,
+                span: span.clone(),
+                message: format!(
+                    "TypeError: '{}' is not a '{}' expression.",
                     offending_token, expected
-                ),
-            );
-            println!("{}\n{}\n", header, frame);
+                )
+                .into(),
+                hint: None,
+            }
         }
         SemanticError::FunctionNotDefined { span, name_span } => {
             let file = &span.file.to_string();
             let source = source_map[file];
-            let start = location_from_offset(source, name_span.start);
-            let end = location_from_offset(source, name_span.end);
-            let header = create_header(file, &start, Level::Error);
-            let error_span = NodeLocation { end, start };
             let offending_token = &source[name_span.start..name_span.end];
-            let frame = create_codeframe(
-                source,
-                error_span,
-                &format!("'{}' is not defined in this scope.", offending_token),
-            );
-            println!("{}\n{}\n", header, frame);
+            DiagnosticMessage {
+                level: Level::Error,
+                span: span.clone(),
+                message: format!("'{}' is not defined in this scope.", offending_token).into(),
+                hint: None,
+            }
         }
-    }
+    };
+
+    let emitter = ErrorEmitter::new(source_map);
+    emitter.emit(&[diagnostic]);
 }
 
 pub fn print_tokenizer_error(error: &TokenizerError, source_map: &HashMap<String, &'static str>) {
-    match error {
+    let diagnostic = match error {
         TokenizerError::UnreconizedToken {
             file,
             token,
             offset,
-        } => {
-            let source = source_map[file];
-            let location = location_from_offset(source, *offset);
-
-            let header = create_header(file, &location, Level::Error);
-            let error_span = NodeLocation {
-                end: Location {
-                    line: location.line,
-                    col: location.col + 1,
-                },
-                start: location,
-            };
-
-            let frame = create_codeframe(
-                source,
-                error_span,
-                &format!("Unrecognized token '{}'", token),
-            );
-
-            println!("{}\n{}\n", header, frame);
-        }
+        } => DiagnosticMessage {
+            level: Level::Error,
+            span: Span::new(file.clone().into(), *offset, *offset + 1),
+            message: format!("SyntaxError: Unrecognized token '{}'.", token).into(),
+            hint: None,
+        },
         TokenizerError::UnexpectedEof => todo!(),
-    }
+    };
+    let emitter = ErrorEmitter::new(source_map);
+    emitter.emit(&[diagnostic]);
 }
 
 pub fn print_parser_error(error: &ParseError, source_map: &HashMap<String, &'static str>) {
-    match error {
+    let diagnostic = match error {
         ParseError::MismatchedToken {
             expected,
             actual,
             span,
-        } => {
-            let file = &span.file.to_string();
-            let source = source_map[file];
-            let start = location_from_offset(source, span.start);
-            let end = location_from_offset(source, span.end);
+        } => DiagnosticMessage {
+            level: Level::Error,
+            span: span.clone(),
+            message: format!(
+                "MismatchedToken: Expected '{}', but found '{}'",
+                expected, actual
+            )
+            .into(),
+            hint: None,
+        },
 
-            let header = create_header(file, &start, Level::Error);
-            let error_span = NodeLocation { end, start };
-            let frame = create_codeframe(
-                source,
-                error_span,
-                &format!(
-                    "MismatchedToken: Expected '{}', but found '{}'",
-                    expected, actual
-                ),
-            );
-
-            println!("{}\n{}\n", header, frame);
-        }
         ParseError::NoViableAlternative {
             expected,
             actual,
             span,
-        } => {
-            let file = &span.file.to_string();
-            let source = source_map[file];
-            let start = location_from_offset(source, span.start);
-            let end = location_from_offset(source, span.end);
-
-            let header = create_header(file, &start, Level::Error);
-            let error_span = NodeLocation { end, start };
-            let frame = create_codeframe(
-                source,
-                error_span,
-                &format!(
-                    "NoViableAlternative: Expected {:?}, but found {:?}",
-                    expected, actual
-                ),
-            );
-            println!("{}\n{}\n", header, frame);
-        }
-    }
-}
-
-fn create_header(file: &str, location: &Location, level: Level) -> String {
-    let err = match level {
-        Level::Error => "error".bright_red().bold(),
-        Level::Warning => "warning".yellow().bold(),
+        } => DiagnosticMessage {
+            level: Level::Error,
+            span: span.clone(),
+            message: format!(
+                "NoViableAlternative: Expected {:?}, but found {:?}",
+                expected, actual
+            )
+            .into(),
+            hint: None,
+        },
     };
-
-    format!("{}: {}:{}:{}", err, file, location.line, location.col)
-}
-
-fn create_codeframe(source: &str, location: NodeLocation, message: &str) -> String {
-    code_frame(&highlight(source, false), location, message)
+    let emitter = ErrorEmitter::new(source_map);
+    emitter.emit(&[diagnostic]);
 }
